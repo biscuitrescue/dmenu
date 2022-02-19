153b8f995a5c7e954a0ebe11bafa1f736c441895
biscuitrescue <karttikeyasinha11@gmail.com>
2022-02-20 02:38:10 +0530

    Changed font back

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 500;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Space mono for powerline:size=12"
+	"Space mono for powerline:size=13"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
