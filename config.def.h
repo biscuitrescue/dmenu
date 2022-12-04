918a46da8765a6c3616d9aa666bebc3e17aa6088
biscuitrescue <karttikeyasinha11@gmail.com>
2022-12-04 13:35:34 +0530

    updated

--- config.def.h
+++ config.def.h
@@ -2,8 +2,12 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;
+static int lines = 10;
+static unsigned int lineheight = 0;
+static unsigned int min_lineheight = 8;
+
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"operator mono medium:size=13"
@@ -11,27 +15,15 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#292d3e" },
-	[SchemeSel] = { "#292d3e", "#86aaff" },
-	[SchemeSelHighlight] = { "#86aaff", "#000000" },
-	[SchemeNormHighlight] = { "#86aaff", "#000000" },
+	[SchemeNorm] = { "#CDD6F4", "#1e1e2e" },
+	[SchemeSel] = { "#1e1e2e", "#B4BEFE" },
+	[SchemeSelHighlight] = { "#B4BEFE", "#000000" },
+	[SchemeNormHighlight] = { "#B4BEFE", "#000000" },
 	[SchemeOut] = { "#000000", "#00ffff" },
-	[SchemeMid] = { "#f3f4f5", "#292d3e" },
-	[SchemeHp] = { "#292d3e", "#bc7cf7" }
+	[SchemeMid] = { "#f3f4f5", "#1e1e2e" },
+	[SchemeHp] = { "#1e1e2e", "#bc7cf7" }
 
 };
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
-/* -h option; minimum height of a menu line */
-static unsigned int lineheight = 0;
-static unsigned int min_lineheight = 8;
-
-
-/*
- * Characters not considered part of a word while deleting words
- * for example: " /?\"&[]"
- */
 static const char worddelimiters[] = " ";
 
-/* Size of the window border */
-static const unsigned int border_width = 5;
+static const unsigned int border_width = 3;
