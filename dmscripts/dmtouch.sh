ef5ca49cd6dd11b6dd5d11fd4e5e27b5f42595ca
biscuitrescue <karttikeyasinha11@gmail.com>
2025-03-19 06:49:42 +0530

    Revert "dmenu -> rofi"
    This reverts commit 11fddeb08ac1bcbd1059811387cfd4cad5f64813.

--- dmscripts/dmtouch.sh
+++ dmscripts/dmtouch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-read file | rofi -dmenu -i -l -p 'File name: '
+read file | dmenu -i -l -p 'File name: '
 
 DMEDITOR="neovide"
--- dmscripts/power.sh
+++ dmscripts/power.sh
@@ -9,7 +9,7 @@ declare -a options=(
     "quit"
 )
 
-choice=$(printf '%s\n' "${options[@]}" | rofi -dmenu -i -l 5 -p 'Are you sure you wanna go?')
+choice=$(printf '%s\n' "${options[@]}" | dmenu -i -l 5 -p 'Are you sure you wanna go?')
 
 if [[ "$choice" == "quit" ]]; then
     exit
